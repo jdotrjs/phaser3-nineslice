66b2dc770f0b9f7e093335f5641e04838e282b11
jdotrjs <jdotrjs@users.noreply.github.com>
2018-06-18 13:02:45 -0700

    order

--- src/types.js
+++ src/types.js
@@ -1,6 +1,21 @@
 // TODO: convert to typescript so that it actually means something <_<
 
 /*
+ * type NineSliceConfig = {
+ *    sourceKey: string,
+ *    sourceFrame?: string || number
+ *    sourceLayout: LayoutConfig | CornerConfig,
+ *    safeOffsets?: UsableSpaceOffsets,
+ *    minSizing?: MinSizeConfig | false,
+ * }
+ *
+ * type PositionConfig = {
+ *    x?: number,
+ *    y?: number,
+ *    width?: number,
+ *    height?: number,
+ * }
+ *
  * type LayoutConfig = {
  *    topLeft: CornerConfig,
  *    topRight?: CornerConfig,
@@ -25,18 +40,4 @@
  *    height: number,
  * }
  *
- * type NineSliceConfig = {
- *    sourceKey: string,
- *    sourceFrame?: string || number
- *    sourceLayout: LayoutConfig | CornerConfig,
- *    safeOffsets?: UsableSpaceOffsets,
- *    minSizing?: MinSizeConfig | false,
- * }
- *
- * type PositionConfig = {
- *    x?: number,
- *    y?: number,
- *    width?: number,
- *    height?: number,
- * }
  */
